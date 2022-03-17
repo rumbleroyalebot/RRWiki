c94ddda1ad9860f27e2b0fd4516c0dcbc53743de
Mohammed Khaled <kingm7md112@gmail.com>
2022-03-17 05:31:37 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,3 +14,32 @@ function toTop() {
   document.body.scrollTop = 0; // Safari
   document.documentElement.scrollTop = 0; // Chrome and Firefox
 }
+
+
+// Importing JSON files:
+
+// Shop Items 1/4:
+
+fetch("databases/shop_items.json")
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    console.log(data);
+    const skins = data["skins"];
+
+    for (var i = 0; i < skins.length; i++) {
+      console.log(skins[i]);
+      document.getElementById("itemList").innerHTML =
+        document.getElementById("itemList").innerHTML +
+        ("Name: " + skins[i].name + "<br />") +
+        ("Skin: " + skins[i].skin + "<br />") +
+        ("Era: " + skins[i].era + "<br />") +
+        ("Cost: " + skins[i].cost + "<br />") +
+        ("Price: " + skins[i].price + "<br /> <br />");
+  }
+});
+
+
+
+
