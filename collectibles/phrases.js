a049a4e5e775ab9d5eb7b3a802f888dd2ad1aa00
Mohammed Khaled <kingm7md112@gmail.com>
2022-04-25 10:07:23 +0200

    Improved responsiveness, imported footer and added 3 team members

--- collectibles/phrases.js
+++ collectibles/phrases.js
@@ -1,22 +1,3 @@
-// Function used for the "back top top" button:
-
-let mybutton = document.getElementById("top");
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function toTop() {
-  document.body.scrollTop = 0; // Safari
-  document.documentElement.scrollTop = 0; // Chrome and Firefox
-}
-
-
-
 // Importing JSON files:
 // Phrases 2.5/4:
 
