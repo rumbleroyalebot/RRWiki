1471d4bda84e67d48566a01ade3371574e35e955
Mohammed Khaled <48189737+MoKhaled112@users.noreply.github.com>
2023-08-14 20:44:46 +0300

    Updates to the home page and bugfixes on all files. Basically a new website.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 let toTop = document.getElementById("top");
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    toTop.style.display = "block";
-  } else {
-    toTop.style.display = "none";
-  }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTop.style.display = "block";
+    } else {
+        toTop.style.display = "none";
+    }
 }
 function toTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
