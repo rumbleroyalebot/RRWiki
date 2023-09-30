52d1311a98313a6fedabbbc19b5150a155b27110
Mohammed Khaled <48189737+MoKhaled112@users.noreply.github.com>
2023-09-30 19:17:43 +0300

    Season 8 update

--- collectibles/banners.js
+++ collectibles/banners.js
@@ -1,47 +1,23 @@
-// Importing JSON files:
-
-// Banners 3/4:
-
-fetch('../databases/banners.json')
-    .then((resp) => {
-        return resp.json();
-    })
+fetch("../databases/banners.json")
+    .then((response) => response.json())
     .then((data) => {
-        // console.log(data);
-        var divArray = new Array();
-        var imageNames = Object.getOwnPropertyNames(data);
-        console.log(imageNames);
-        for (let i in imageNames) {
-            // get image name and link
-            var Name = imageNames[i];
-            var Link = data[imageNames[i]];
-
-            // make elements
-            var divID = Name.toLowerCase().replace(' ', '_').replace('.', '_');
-            var imageDiv = document
-            .getElementById('banners')
-            .appendChild(document.createElement('div'));
-            imageDiv.id = divID;
-            imageDiv.classList.add('bannerContainer');
-            divArray.push(divID);
-
-            let imageName = imageDiv.appendChild(document.createElement('p'));
-            imageName.appendChild(document.createTextNode(Name));
-            imageName.classList.add('bannerName');
-
-            var Image = imageDiv.appendChild(document.createElement('img'));
-            Image.setAttribute('src', Link);
-            Image.setAttribute('loading', 'lazy');
-            Image.setAttribute('draggable', 'false')
-            Image.classList.add('banner');
-
-            document.getElementById('banners').appendChild(document.createElement('hr'));
-
-            // event listener
-            imageDiv.addEventListener('click', () => {
-                document
-                    .getElementById(divArray[i])
-                    .children[1].classList.toggle('bigimage');
-            });
+        const bannerSection = document.getElementById("banner-section");
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                const banner = data[key];
+                const card = document.createElement("article");
+                card.classList.add("card");
+                const img = document.createElement("img");
+                img.classList.add("card-img");
+                img.src = banner.image;
+                img.alt = "";
+                const cardText = document.createElement("p");
+                cardText.classList.add("card-text");
+                cardText.textContent = banner.name;
+                card.appendChild(img);
+                card.appendChild(cardText);
+                bannerSection.appendChild(card);
+            }
         }
-    });
+    })
+    .catch((error) => console.error("Error loading JSON:", error));
--- collectibles/skins.js
+++ collectibles/skins.js
@@ -4,163 +4,230 @@
 
 // Classic Era 1/8:
 fetch("../databases/shop_items.json")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((data) => {
-    var skins = data["skins"];
-    var skindiv = document.getElementById("classicSkins");
-    for (let i in skins) {
-      if (skins[i]["era"] === "classic") {
-        skindiv.innerHTML =
-          skindiv.innerHTML +
-          ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-          ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-      }
-    }
-  });
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("classicSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "classic") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
 // Modern Era 2/8:
 fetch("../databases/shop_items.json")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((data) => {
-    var skins = data["skins"];
-    var skindiv = document.getElementById("modernSkins");
-    for (let i in skins) {
-      if (skins[i]["era"] === "modern") {
-        skindiv.innerHTML =
-          skindiv.innerHTML +
-          ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-          ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-      }
-    }
-  });
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("modernSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "modern") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
 // Futuristic Era 3/8:
 fetch("../databases/shop_items.json")
-.then((resp) => {
-  return resp.json();
-})
-.then((data) => {
-  var skins = data["skins"];
-  var skindiv = document.getElementById("futuristicSkins");
-  for (let i in skins) {
-    if (skins[i]["era"] === "futuristic") {
-      skindiv.innerHTML =
-        skindiv.innerHTML +
-        ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-        ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-    }
-  }
-});
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("futuristicSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "futuristic") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
 // Medieval Era 4/8:
 fetch("../databases/shop_items.json")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((data) => {
-    var skins = data["skins"];
-    var skindiv = document.getElementById("medievalSkins");
-    for (let i in skins) {
-      if (skins[i]["era"] === "medieval") {
-        skindiv.innerHTML =
-          skindiv.innerHTML +
-          ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-          ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-      }
-    }
-  });
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("medievalSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "medieval") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
 // Pirate Era 5/8:
 fetch("../databases/shop_items.json")
-.then((resp) => {
-  return resp.json();
-})
-.then((data) => {
-  var skins = data["skins"];
-  var skindiv = document.getElementById("pirateSkins");
-  for (let i in skins) {
-    if (skins[i]["era"] === "pirate") {
-      skindiv.innerHTML =
-        skindiv.innerHTML +
-        ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-        ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-    }
-  }
-});
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("pirateSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "pirate") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
 // Samurai Era 6/8:
 fetch("../databases/shop_items.json")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((data) => {
-    var skins = data["skins"];
-    var skindiv = document.getElementById("samuraiSkins");
-    for (let i in skins) {
-      if (skins[i]["era"] === "samurai") {
-        skindiv.innerHTML =
-          skindiv.innerHTML +
-          ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-          ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-      }
-    }
-  });
-
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("samuraiSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "samurai") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
 // Steampunk Era 7/8:
 fetch("../databases/shop_items.json")
-.then((resp) => {
-  return resp.json();
-})
-.then((data) => {
-  var skins = data["skins"];
-  var skindiv = document.getElementById("steampunkSkins");
-  for (let i in skins) {
-    if (skins[i]["era"] === "steampunk") {
-      skindiv.innerHTML =
-        skindiv.innerHTML +
-        ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-        ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-    }
-  }
-});
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("steampunkSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "steampunk") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
-// Zombie Era 6/8:
+// Zombie Era 8/8:
 fetch("../databases/shop_items.json")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((data) => {
-    var skins = data["skins"];
-    var skindiv = document.getElementById("zombieSkins");
-    for (let i in skins) {
-      if (skins[i]["era"] === "zombie") {
-        skindiv.innerHTML =
-          skindiv.innerHTML +
-          ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
-          ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
-      }
-    }
-  });
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("zombieSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "zombie") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
+
+// Magical Era 9:
+fetch("../databases/shop_items.json")
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("magicalSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "magical") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
+
+// Egyptian Era 8/8:
+fetch("../databases/shop_items.json")
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        var skins = data["skins"];
+        var skindiv = document.getElementById("egyptianSkins");
+        for (let i in skins) {
+            if (skins[i]["era"] === "egyptian") {
+                skindiv.innerHTML =
+                    skindiv.innerHTML +
+                    ("<p>Name:<span class = 'gold'> " + skins[i]["skin"] + " " + skins[i]["item"] + "</span><br />") +
+                    ("Cost: " + skins[i]["price"] + " " + skins[i]["cost"] + "</p>");
+            }
+        }
+    });
 
 // Old Event Shop Items 1.5/4:
 
 fetch("../databases/shop_items.json")
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((data) => {
-    const old_event = data["old_event"];
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        const old_event = data["christmas"];
 
-  for (var i = 0; i < old_event.length; i++) {
-  document.getElementById("OldEventsitemList").innerHTML =
-    document.getElementById("OldEventsitemList").innerHTML +
-    ("<p> Name: <span class = 'gold'>" + old_event[i].skin + " " + old_event[i].item + "</span> <br>") +
-    ("Era: <b>" + old_event[i].era + "</b><br>") +
-    ("Price: " + old_event[i].price + " " + old_event[i].cost + "</p>");
-  }
-});
+        for (var i = 0; i < old_event.length; i++) {
+            document.getElementById("christmasEvent").innerHTML =
+                document.getElementById("christmasEvent").innerHTML +
+                ("<p> Name: <span class = 'gold'>" + old_event[i].skin + " " + old_event[i].item + "</span> <br>") +
+                ("Era: <b>" + old_event[i].era + "</b><br>") +
+                ("Price: " + old_event[i].price + " " + old_event[i].cost + "</p>");
+        }
+    });
+
+fetch("../databases/shop_items.json")
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        const old_event = data["halloween"];
+
+        for (var i = 0; i < old_event.length; i++) {
+            document.getElementById("halloweenEvent").innerHTML =
+                document.getElementById("halloweenEvent").innerHTML +
+                ("<p> Name: <span class = 'gold'>" + old_event[i].skin + " " + old_event[i].item + "</span> <br>") +
+                ("Era: <b>" + old_event[i].era + "</b><br>") +
+                ("Price: " + old_event[i].price + " " + old_event[i].cost + "</p>");
+        }
+    });
+
+fetch("../databases/shop_items.json")
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        const old_event = data["old_old_event"];
+
+        for (var i = 0; i < old_event.length; i++) {
+            document.getElementById("OldEventsitemList").innerHTML =
+                document.getElementById("OldEventsitemList").innerHTML +
+                ("<p> Name: <span class = 'gold'>" + old_event[i].skin + " " + old_event[i].item + "</span> <br>") +
+                ("Era: <b>" + old_event[i].era + "</b><br>") +
+                ("Price: " + old_event[i].price + " " + old_event[i].cost + "</p>");
+        }
+    });
